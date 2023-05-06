fa3f392fa2a2ff72fe7adbb5113cd4d7a97005d1
ayoniyi <ayo.a.niyi@gmail.com>
2023-05-05 23:32:50 -0700

    rm key

--- src/services/article.js
+++ src/services/article.js
@@ -1,18 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const rapidApiKey = import.meta.env.VITE_RAPID_ARTICLE_KEY;
-// const options = {
-//   method: "GET",
-//   url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
-//   params: {
-//     url: "https://time.com/6266679/musk-ai-open-letter/",
-//     length: "3",
-//   },
-//   headers: {
-//     "X-RapidAPI-Key": "b089d6e398msh3f385ed7b744133p1eb1b3jsn9dc7c982d37a",
-//     "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
-//   },
-// };
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
